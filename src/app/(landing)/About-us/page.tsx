7511b14534b335087ba3c461d6b0a39ed5fbb198
0xnehasingh <nehasingh2mau@gmail.com>
2024-08-23 21:11:41 +0530

    added details about the landing page

---
+++ src/app/(landing)/About-us/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const ContactUs: React.FC = () => {
+
+	return (
+		<section className="min-h-screen bg-gradient-to-r from-blue-100 to-purple-100 flex items-center justify-center p-6">
+		</section>
+	);
+};
+
+export default ContactUs;
