eb241156571977c71781179cc3bc4fdd67e52326
0xnehasingh <nehasingh2mau@gmail.com>
2024-08-24 21:15:39 +0530

    styling injection in AntdRegistry component

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import GoHomesLandingPage from '../_components/LandingPage/landing-page';
+
+export default function HomePage() {
+	return (
+		<main>
+			<GoHomesLandingPage/>
+		</main>
+	);
+}
+
